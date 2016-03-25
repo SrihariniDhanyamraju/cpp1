afd775f21d54ec36b56d1227fd4fa2bfba5a9d90
sriharini dhanyamraju <bossmool@boss>
2016-03-25 15:47:47 +0530

    cpp1

---
+++ sample1.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+using namespace std;
+main()
+{
+ int a,b;
+cout<<"enter the values of a and b:";
+cin>>a>>b;
+cout<<"the product is:"<<(a*b);
+cout<<"the difference is:"<<(a-b);
+}
